0926a5d73b6c3c23ae99863bbf7d912c16ccf68d
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-07 21:09:26 -0300

    first commit with nextjs

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home(){
+    const message = (e) => {
+        document.getElementById("dial").showModal()
+    }
+    return (
+        <div>
+        <h3>Você tem uma mensagem especial:</h3>
+        <dialog id="dial">
+            Eu te amo Arlene, de: Felipe
+        </dialog>
+
+        <button onClick={message}>Ver mensagem</button>
+        </div>
+    )
+}
+
+export default Home;
