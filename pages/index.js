ed60bbac65851641fcf68bb7fcedc6a45394afa8
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-08 20:20:02 -0300

    update the identation of the HTML

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ function Home(){
 
     return (
         <div>
-        <h3>Você tem uma mensagem especial:</h3>
-        <dialog id="dial">
-            Eu te amo Arlene, de: Felipe
-        </dialog>
-        <button onClick={message}>Ver mensagem</button>
-        </ div>
+            <h3>Você tem uma mensagem especial:</h3>
+            <dialog id="dial">
+                Eu te amo Arlene, de: Felipe
+            </dialog>
+
+            <button onClick={message}>Ver mensagem</button>
+        </div>
     )
 }
 
