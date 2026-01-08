0d44a829f48b4a40a8867bafd95520ea35e34821
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-08 20:14:28 -0300

    removed blank space

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ function Home(){
     const message = (e) => {
         document.getElementById("dial").showModal()
     }
+
     return (
         <div>
         <h3>Você tem uma mensagem especial:</h3>
         <dialog id="dial">
             Eu te amo Arlene, de: Felipe
         </dialog>
-
         <button onClick={message}>Ver mensagem</button>
-        </div>
+        </ div>
     )
 }
 
