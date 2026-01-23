41fc9039e726e158dd73f4d338c0360018df35c0
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-23 19:21:41 -0300

    added the api status v1 and the tests for it, also removed the module calc with it specific test

--- models/calc.js
+++
@@ -1,7 +0,0 @@
-function sumfunc(num1, num2) {
-  if (typeof num1 !== "number") return "Error";
-  if (typeof num2 !== "number") return "Error";
-  return num1 + num2;
-}
-
-exports.sumfunc = sumfunc;
