4fb14272ef2d8d1cb003f78245b21b59baf32a44
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-29 20:49:00 -0300

    tests updates

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -33,4 +33,13 @@ test("GET to /api/v1/status data from database must have server_version | max_co
   const { body } = await getStatus();
 
   expect(body.database.server_version).toBeTruthy();
+  expect(body.database.server_version).toBe('18.1');
+});
+
+test("GET to /api/v1/status data from database must have max_connections", async () => {
+  expect(body.database.max_connections).toBeTruthy();
+});
+
+test("GET to /api/v1/status data from database must have current_connections", async () => {
+  expect(body.database.current_connections).toBeTruthy();
 });
