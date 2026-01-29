f4cb458f773d3c98d1684ba731609b71f8843a0a
cristianofmc <cristiano.fmc@hotmail.com>
2026-01-29 19:32:28 -0300

    added RAII pattern

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,21 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
-async function query(queryObject){
+export async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: Number(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  try {
+    return await client.query(queryObject);
+  } finally {
+    await client.end().catch(() => {});
+  }
 }
 
-export default {
-  query: query
-};
+export default { query };
