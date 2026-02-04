2a67cb2e172fc895653868aaa0f709ae995054a4
cristianofmc <cristiano.fmc@hotmail.com>
2026-02-04 07:20:51 -0300

    update on migrations .run

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,15 @@
-import migrationRunner from 'node-pg-migrate'
+import { runner as migrationRunner } from 'node-pg-migrate'
+import { join } from "node:path"
 
 async function migrations(request, response) {
   const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    direction: "up",
+    dir: join("infra", "migrations"),
+    verbose: true,
   });
+
   response.status(200).json(migrations);
 }
 
