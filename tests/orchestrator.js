dffb5175b91e4972170d3ab021753be1ec8cd1d4
cristianofmc <cristiano.fmc@hotmail.com>
2026-03-21 14:25:56 -0300

    added fix for fetch error

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -10,8 +10,10 @@ async function waitForAllServices() {
     });
 
     async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      const response = await fetch("http://localhost:3000/api/v1/statuss");
+      if (!response.ok){
+        throw new Error();
+      }
     }
   }
 }
