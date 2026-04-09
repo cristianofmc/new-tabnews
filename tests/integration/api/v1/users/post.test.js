8dd450c7c3c2cf2d781648d0254d6b3c1ba2ec9b
cristianofmc <cristiano.fmc@hotmail.com>
2026-04-09 09:36:55 -0300

    feat: create user model and api/v1/users endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
 import orchestrator from "#tests/orchestrator.js";
-import database from "#infra/database.js";
+import { version as uuidVersion } from "uuid";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,27 +10,106 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("should create a new user account with unique and valid data", async () => {
-      // const { response } = await orchestrator.request("/api/v1/users", {
-      //   method: "POST",
-      //   body: JSON.stringify({
-      //     username: "jimmyfive",
-      //   }),
-      // });
-
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["jimmyfive", "jimmyfive@host.testemail", "Test@123"],
+      const { response, body } = await orchestrator.request("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "jimmyfive",
+          email: "jimmyfive@host.testemail",
+          password: "Test@123",
+        }),
       });
 
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["jimmyfive2", "Jimmyfive@host.testemail", "Test@123"],
+      expect(response.status).toBe(201);
+
+      expect(body).toEqual({
+        id: body.id,
+        username: "jimmyfive",
+        email: "jimmyfive@host.testemail",
+        password: "Test@123",
+        created_at: body.created_at,
+        updated_at: body.updated_at,
       });
 
-      const users = await database.query("SELECT * FROM users;");
-      console.log(users.rows);
+      expect(uuidVersion(body.id)).toBe(4);
+      expect(Date.parse(body.created_at)).not.toBe(NaN);
+      expect(Date.parse(body.updated_at)).not.toBe(NaN);
+    });
 
-      // expect(response.status).toBe(201);
+    test("should not create a new user account with duplicated email", async () => {
+      const returnRequest = await orchestrator.request("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "first_jimmyfive",
+          email: "duplicated_email_jimmyfive@host.testemail",
+          password: "Test@123",
+        }),
+      });
+
+      expect(returnRequest.response.status).toBe(201);
+
+      const returnRequest2 = await orchestrator.request("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "second_jimmyfive",
+          email: "Duplicated_email_jimmyfive@host.testemail",
+          password: "Test@123",
+        }),
+      });
+
+      expect(returnRequest2.response.status).toBe(400);
+
+      expect(returnRequest2.body).toEqual({
+        name: "ValidationError",
+        message: "The email address provided is already registered.",
+        action: "Try again with a different email.",
+        status_code: 400,
+      });
+    });
+
+    test("should not create a new user account with duplicated username", async () => {
+      const returnRequest = await orchestrator.request("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicated_jimmyfive",
+          email: "first_duplicated_username_jimmyfive@host.testemail",
+          password: "Test@123",
+        }),
+      });
+
+      expect(returnRequest.response.status).toBe(201);
+
+      const returnRequest2 = await orchestrator.request("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "Duplicated_jimmyfive",
+          email: "second_duplicated_username_jimmyfive@host.testemail",
+          password: "Test@123",
+        }),
+      });
+
+      expect(returnRequest2.response.status).toBe(400);
+
+      expect(returnRequest2.body).toEqual({
+        name: "ValidationError",
+        message: "The username provided is already registered.",
+        action: "Try again with a different username.",
+        status_code: 400,
+      });
     });
   });
 });
