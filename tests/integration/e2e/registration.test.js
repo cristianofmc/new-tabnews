3c6ce5574cf490f323d1210d0cf208697f0bcbc2
cristianofmc <cristiano.fmc@hotmail.com>
2026-04-26 22:09:46 -0300

    test: add test for user registration e2e flow

---
+++ tests/integration/e2e/registration.test.js
@@ -0,0 +1,43 @@
+import orchestrator from "#tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.cleanDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("E2E registration happy path", () => {
+  test("Create user account", async () => {
+    const { response, body } = await orchestrator.request("/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "NewJimmyfive",
+        email: "newjimmyfive@host.testemail",
+        password: "Test@123",
+      }),
+    });
+
+    expect(response.status).toBe(201);
+
+    expect(body).toEqual({
+      id: body.id,
+      username: "NewJimmyfive",
+      email: "newjimmyfive@host.testemail",
+      password: body.password,
+      created_at: body.created_at,
+      updated_at: body.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {});
+
+  test("Activate account", async () => {});
+
+  test("Sign in", async () => {});
+
+  test("Get user information", async () => {});
+});
