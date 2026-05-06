47084c67ca9bee39bf788542ca2b42b69928938d
cristianofmc <cristiano.fmc@hotmail.com>
2026-05-06 20:20:16 -0300

    fix: fix a bug for session and activation_token

--- models/authorization.js
+++ models/authorization.js
@@ -41,7 +41,7 @@ function filterOutput(user, feature, resource) {
   }
 
   if (feature === "read:session") {
-    if (user.id === resource.id) {
+    if (user.id === resource.user_id) {
       return {
         id: resource.id,
         token: resource.token,
@@ -54,16 +54,14 @@ function filterOutput(user, feature, resource) {
   }
 
   if (feature === "read:activation_token") {
-    if (user.id === resource.id) {
-      return {
-        id: resource.id,
-        user_id: resource.user_id,
-        created_at: resource.created_at,
-        updated_at: resource.updated_at,
-        expires_at: resource.expires_at,
-        used_at: resource.used_at,
-      };
-    }
+    return {
+      id: resource.id,
+      user_id: resource.user_id,
+      created_at: resource.created_at,
+      updated_at: resource.updated_at,
+      expires_at: resource.expires_at,
+      used_at: resource.used_at,
+    };
   }
 }
 
